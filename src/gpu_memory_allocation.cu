41192a4d0d1007294a3c219f200143bd99b49c64
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-11 23:13:57 +0300

    git script is added

--- src/gpu_memory_allocation.cu
+++ src/gpu_memory_allocation.cu
@@ -30,8 +30,8 @@ void malloc_graph_gpu_memory(Graph &g,G_pointers &p){
     chkerr(cudaMemcpy(p.neighbors_offset,g.neighbors_offset,(g.V+1)*sizeof(unsigned int),cudaMemcpyHostToDevice));
     chkerr(cudaMalloc(&(p.degrees),(g.V)*sizeof(unsigned int)));
     chkerr(cudaMemcpy(p.degrees,g.degrees,(g.V)*sizeof(unsigned int),cudaMemcpyHostToDevice));
-    std::cout<<"memory graph g = "<<g.neighbors[0]<<endl;
-    std::cout<<"memory graph p = "<<p.neighbors[0]<<endl;
+    std::cout<<"memory graph g = "<<g.degrees[0]<<endl;
+    // std::cout<<"memory graph p = "<<p.neighbors[0]<<endl;
 }
 
 void get_results_from_gpu(Graph &g,G_pointers &p){
