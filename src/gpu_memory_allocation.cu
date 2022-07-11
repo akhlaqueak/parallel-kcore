217eab7d09e4ec35cea837e54383bb3d62669fc9
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-11 22:50:48 +0300

    fixing errors

--- src/gpu_memory_allocation.cu
+++ src/gpu_memory_allocation.cu
@@ -30,9 +30,7 @@ void malloc_graph_gpu_memory(Graph &g,G_pointers &p){
     chkerr(cudaMemcpy(p.neighbors_offset,g.neighbors_offset,(g.V+1)*sizeof(unsigned int),cudaMemcpyHostToDevice));
     chkerr(cudaMalloc(&(p.degrees),(g.V)*sizeof(unsigned int)));
     chkerr(cudaMemcpy(p.degrees,g.degrees,(g.V)*sizeof(unsigned int),cudaMemcpyHostToDevice));
-    chkerr(cudaMalloc(&(p.V),sizeof(unsigned int)));
-    chkerr(cudaMemset(p.V,g.V,sizeof(unsigned int)));
-    std::cout<<"p = "<<p.V[0]<<"g = "<<g.V<<endl;
+    std::cout<<"memory graph g = "<<g.V<<endl;
 }
 
 void get_results_from_gpu(Graph &g,G_pointers &p){
