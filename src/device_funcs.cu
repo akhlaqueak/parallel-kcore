f942bdede10c9730fc20a2b0d5760f9a31cf213a
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-11 20:02:28 +0300

    fixing errors

--- src/device_funcs.cu
+++ src/device_funcs.cu
@@ -74,11 +74,11 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level){
                 a = atomicSub(&d_p.degrees[u], 1);
             }
 
-            if(a == (level+1)){
-                int loc = warp_id*MAX_NE + e[warp_id];
-                buffer[loc] = u;
-                atomicAdd(&e[warp_id], 1);
-            }
+            // if(a == (level+1)){
+            //     int loc = warp_id*MAX_NE + e[warp_id];
+            //     buffer[loc] = u;
+            //     atomicAdd(&e[warp_id], 1);
+            // }
 
             if(a <= level){
                 atomicAdd(&d_p.degrees[u], 1);
