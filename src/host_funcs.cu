6af0d25d6542a758fc1d90dba450f1e976f1de1b
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-11 20:48:22 +0300

    fixing errors

--- src/host_funcs.cu
+++ src/host_funcs.cu
@@ -86,30 +86,30 @@ void find_kcore(string data_file,bool write_to_disk){
 }
 
 void find_kcore_CPU(string data_file,bool write_to_disk){
-    cout<<"start loading graph file from disk to memory..."<<endl;    
-    Graph data_graph(data_file);
-    cout<<"graph loading complete..."<<endl;
+    // cout<<"start loading graph file from disk to memory..."<<endl;    
+    // Graph data_graph(data_file);
+    // cout<<"graph loading complete..."<<endl;
 
-    int level = 0;
-    int count = 0;
+    // int level = 0;
+    // int count = 0;
 
-    while(count<data_graph.V){
-        for(int i=0; i<data_graph.V; i++){
-            if (degrees[i] == level){
-                int start = data_graph.neighbors_offset[v];
-                int end = data_graph.neighbors_offset[v+1];
+    // while(count<data_graph.V){
+    //     for(int i=0; i<data_graph.V; i++){
+    //         if (degrees[i] == level){
+    //             int start = data_graph.neighbors_offset[v];
+    //             int end = data_graph.neighbors_offset[v+1];
 
-                for(int j = start; j<end; j++){
-                    int a = 0;
-                    unsigned int u = data_graph.neighbors[j];
-                    if(data_graph.degrees[u] > level){
-                        data_graph.degrees[u] --;
-                        a = data_graph.degrees[u];
-                    }
+    //             for(int j = start; j<end; j++){
+    //                 int a = 0;
+    //                 unsigned int u = data_graph.neighbors[j];
+    //                 if(data_graph.degrees[u] > level){
+    //                     data_graph.degrees[u] --;
+    //                     a = data_graph.degrees[u];
+    //                 }
 
-                }
+    //             }
                
-            }
-        }    
-    }
+    //         }
+    //     }    
+    // }
 }
