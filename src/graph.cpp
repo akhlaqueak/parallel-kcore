c0825a3d97ec6a15bb999024257edf79c1ecb3a9
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-21 00:59:38 +0300

    parallel loops are done

--- src/graph.cpp
+++ src/graph.cpp
@@ -1,6 +1,7 @@
 
 #include "../inc/graph.h"
 
+
 Graph::Graph(std::string input_file){
     
     vector< set<unsigned int> > ns;
@@ -8,27 +9,35 @@ Graph::Graph(std::string input_file){
     degrees = new unsigned int[V];
 
 
-    neighbors_offset = new unsigned int[V+1];
-    neighbors_offset[0] = 0;
     
     #pragma omp parallel for
     for(int i=0;i<V;++i){
         degrees[i] = ns[i].size();
-        neighbors_offset[i+1] = neighbors_offset[i] + ns[i].size();
+        // neighbors_offset[i+1] = neighbors_offset[i] + degrees[i];
     }
+
+    neighbors_offset = new unsigned int[V+1];
+    neighbors_offset[0] = 0;
+    partial_sum(degrees, degrees+V, neighbors_offset+1);
 
     E = neighbors_offset[V];
-    neighbors = new unsigned int[neighbors_offset[V]];
+    neighbors = new unsigned int[E];
+
+    #pragma omp parallel for
+    for(int i=0;i<V;i++){
+        for(int j=neighbors_offset[i], auto it = ns[i].begin(); j < neighbors_offset[i+1]; j++, it++)
+            neighbors[j] = *it;
+    }
     // AVG_degrees = E/V + 2;
 
-    unsigned int j = 0;
-    for(unsigned int i=0;i<V;++i){
-        std::set<unsigned int> s = ns[i];
-        for(std::set<unsigned int>::iterator p = s.begin();p!=s.end();p++){
-            neighbors[j] = *p;
-            j++;
-        }
-    }
+    // unsigned int j = 0;
+    // for(unsigned int i=0;i<V;++i){
+    //     std::set<unsigned int> s = ns[i];
+    //     for(std::set<unsigned int>::iterator p = s.begin();p!=s.end();p++){
+    //         neighbors[j] = *p;
+    //         j++;
+    //     }
+    // }
 }
 
 Graph::~Graph(){
