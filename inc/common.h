b093585ee3f83f98168420a08c3ef942c4769ae1
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-22 13:44:37 +0300

    global buffer added

--- inc/common.h
+++ inc/common.h
@@ -7,6 +7,7 @@
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
 #define MAX_NV 200
 #define N_THREADS (BLK_DIM*BLK_NUMS)
+#define HELPER_SIZE 20000
 #include <iostream>
 #include <vector>
 #include <set>
--- src/device_funcs.cu
+++ src/device_funcs.cu
@@ -3,7 +3,7 @@
 #include "stdio.h"
 
 
-__device__ void scan(unsigned int *degrees, unsigned int V, unsigned int* buffer, unsigned int* e, unsigned int level){
+__device__ void scan(unsigned int *degrees, unsigned int V, unsigned int* w_buffer, unsigned int* e, unsigned int level){
     unsigned int warp_id = threadIdx.x/32;
     unsigned int global_threadIdx = blockIdx.x * blockDim.x + threadIdx.x; 
     for(unsigned int i=global_threadIdx; i< V; i+= N_THREADS){
@@ -13,9 +13,20 @@ __device__ void scan(unsigned int *degrees, unsigned int V, unsigned int* buffer
         }
 
             //store this node to shared buffer, at the corresponding warp location
-            unsigned int loc = atomicAdd(&e[warp_id], 1); 
-            loc = loc + warp_id*MAX_NV; 
-            buffer[loc] = i;
+            unsigned int loc = atomicAdd(&e[warp_id], 1);
+
+            if(loc == MAX_NV){
+                helpers[warp_id] = (int*) malloc(HELPER_SIZE);
+            }
+
+            if(loc >= MAX_NV){
+                loc-= MAX_NV;
+                helpers[warp_id][loc] = i;
+            }
+
+            else{
+                buffer[loc] = i;
+            }
         }
     }
 }
@@ -25,7 +36,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
 
     __shared__ unsigned int buffer[WARPS_EACH_BLK*MAX_NV];
     __shared__ unsigned int e[WARPS_EACH_BLK];
-
+    __shared__ unsigned int* helpers[WARPS_EACH_BLK];
 
 
     unsigned int warp_id = threadIdx.x/32;
@@ -34,18 +45,26 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
   //  unsigned int global_idx = (blockIdx.x)*WARPS_EACH_BLK+warp_id;
   //  unsigned int mask = 0xFFFFFFFF;
 
-    if(lane_id==0)
+    if(lane_id==0){
         e[warp_id] = 0;
+        helper[warp_id] = NULL;
+    }
 	
 
     __syncwarp();
 
-    scan(d_p.degrees, V, buffer, e, level);
+    scan(d_p.degrees, V, &buffer[warp_id*MAX_NV], e, level);
 
 
     for(int i=0; i<e[warp_id]; i++){
     
-        unsigned int v = buffer[warp_id*MAX_NV + i];
+        unsigned int v;
+        if( i < MAX_NV ) 
+            v = buffer[warp_id*MAX_NV + i];
+        else
+            v = helpers[warp_id][i-MAX_NV];
+
+
         unsigned int start = d_p.neighbors_offset[v];
         unsigned int end = d_p.neighbors_offset[v+1];
 
@@ -58,8 +77,20 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
                 if(a == (level+1)){
         // node degree became the level after decrementing... 
                     unsigned int loc = atomicAdd(&e[warp_id], 1); 
-                    loc = loc + warp_id*MAX_NV;
-                    buffer[loc] = u;           
+
+                    
+                    if(loc == MAX_NV){
+                        helpers[warp_id] = (int*) malloc(HELPER_SIZE);
+                    }
+
+                    if(loc >= MAX_NV){
+                        loc-= MAX_NV;
+                        helpers[warp_id][loc] = i;
+                    }
+
+                    else{
+                        buffer[warp_id*MAX_NV + loc] = i;
+                    }          
                 }
 
                 if(a <= level){
@@ -72,9 +103,10 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
         __syncwarp();
     }
 
-
     if(lane_id == 0 && e[warp_id]!=0 ){
-        atomicAdd(&global_count[0], e[warp_id]);    
+        atomicAdd(&global_count[0], e[warp_id]);
+        if(helpers[warp_id]!=NULL)
+            free(helpers[warp_id]);  
 	}
 
 }
