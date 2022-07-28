e151d4fcc76a438a491315cab79d628b5cfdd7bb
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-28 16:58:56 +0300

    try block level

--- src/device_funcs.cu
+++ src/device_funcs.cu
@@ -57,7 +57,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
 
     __syncthreads();
 
-    if(THID == 0) printf("%d ", e);
+    if(THID == 0) printf("%d*%d ", blockIdx.x, e);
 
     // e is being incrmented within the loop, 
     // warps should process all the nodes added during the execution of loop
