868e070f4299747f3e3b49ffd21f721c73fb84ab
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-29 01:27:17 +0300

    copied inc files

---
+++ warp-level-atomic/inc/graph.h
@@ -0,0 +1,17 @@
+
+
+#ifndef CUTS_GRAPH_H
+#define CUTS_GRAPH_H
+#include "./util.h"
+class Graph{
+public:
+    unsigned int V;
+    unsigned int E;
+    unsigned int AVG_DEGREE = 0;
+    unsigned int * neighbors;
+    unsigned int * neighbors_offset;
+    unsigned int * degrees;
+    Graph(std::string input_file);
+    ~Graph();
+};
+#endif //CUTS_GRAPH_H
