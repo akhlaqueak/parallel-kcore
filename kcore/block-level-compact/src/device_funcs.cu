5688ce236b215d022a9dae2b4e50436540f7fd29
akhlaqueak <akhlaque.ak@gmail.com>
2022-07-30 01:30:07 +0300

    trying to fix output of block level compact

--- kcore/block-level-compact/src/device_funcs.cu
+++ kcore/block-level-compact/src/device_funcs.cu
@@ -126,6 +126,8 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
     __syncthreads();
 
     selectNodesAtLevel(d_p.degrees, V, buffer, &helper, &e, level);
+
+    if(level == 1) printf("%d ", e);
     
     // e is being incremented within the loop, 
     // warps should process all the nodes added during the execution of loop
