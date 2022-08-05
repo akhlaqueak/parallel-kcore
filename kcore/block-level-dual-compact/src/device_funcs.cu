c6eb06a2cbb16f889c4ae245726c317e636dea13
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-05 16:17:44 +0300

    trying warp level compact at processing

--- kcore/block-level-dual-compact/src/device_funcs.cu
+++ kcore/block-level-dual-compact/src/device_funcs.cu
@@ -151,8 +151,11 @@ __device__ inline void allocateMemory(volatile unsigned int** glBufferPtr){
 }
 
 __device__ void allocateMemoryMutex(volatile unsigned int** glBufferPtr, unsigned int loc, unsigned int* lock){
-    if(atomicExch(lock, 1) == 0)
+    if(atomicExch(lock, 1) == 0){
+        
+        printf("allocated");
         allocateMemory(glBufferPtr);
+    }
     while(glBufferPtr[0] == NULL);
 }    
 
