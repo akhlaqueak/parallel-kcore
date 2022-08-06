fe9df997f5bf7b1da7e80e64e95c19d74c2a5547
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-06 19:00:40 +0300

    trying warp level compact at processing

--- kcore/block-level-dual-compact/src/device_funcs.cu
+++ kcore/block-level-dual-compact/src/device_funcs.cu
@@ -171,8 +171,8 @@ __device__ void synchronizeBlocks(volatile unsigned int* blockCounter){
     }
     
     if(THID==0){
+        while(blockCounter[0]<BLK_NUMS)// busy wait until all blocks increment
         printf("%d ", blockCounter[0]);
-        while(blockCounter[0]<BLK_NUMS);// busy wait until all blocks increment
     }
     __syncthreads();
 }
