5a34f39282d0a23d60ab2c18b817aac90947afdf
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-09 20:59:13 +0300

    implemented hellis scan

--- kcore/block-level-dual-compact/src/device_funcs.cu
+++ kcore/block-level-dual-compact/src/device_funcs.cu
@@ -44,7 +44,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
 
     __syncthreads();
 
-    
+    return;
     // bufTail is being incremented within the loop, 
     // warps should process all the nodes added during the execution of loop
     // for that purpose base is introduced, is incremented whenever a warp takes a job.
