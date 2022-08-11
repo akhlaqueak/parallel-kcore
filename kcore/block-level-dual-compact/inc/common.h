879f056ab0bfe0e1b76814e5784c72bde50e0b16
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-11 14:56:34 +0300

    atmoic checking

--- kcore/block-level-dual-compact/inc/common.h
+++ kcore/block-level-dual-compact/inc/common.h
@@ -1,7 +1,7 @@
 
 #ifndef CUTS_COMMON_H
 #define CUTS_COMMON_H
-#define BLK_NUMS 16
+#define BLK_NUMS 10
 #define BLK_DIM 1024
 #define WARPS_EACH_BLK (BLK_DIM/32)
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
