60d2fe8f282c81e85075789ce3728158e4d4c05d
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-11 15:11:19 +0300

    atmoic checking

--- kcore/block-level-dual-compact/src/device_funcs.cu
+++ kcore/block-level-dual-compact/src/device_funcs.cu
@@ -95,7 +95,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
             if(j >= end) continue;
 
             unsigned int u = d_p.neighbors[j];
-            if(ldg(d_p.degrees+u) > level){
+            if(d_p.degrees[u] > level){
                 unsigned int a = atomicSub(d_p.degrees+u, 1);
                 
                 if(a == level+1){
@@ -109,7 +109,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
                     // node degree became less than the level after decrementing... 
                     atomicAdd(d_p.degrees+u, 1);
                 }
-                __threadfence();
+                // __threadfence();
             }
         }        
     }
