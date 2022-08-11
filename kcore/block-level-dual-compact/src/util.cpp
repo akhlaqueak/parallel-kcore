74b178681d713fdfd9690557f443caa6de28aadc
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-12 00:55:16 +0300

    wronte files in json dict format

--- kcore/block-level-dual-compact/src/util.cpp
+++ kcore/block-level-dual-compact/src/util.cpp
@@ -8,7 +8,7 @@ unsigned  int file_reader(std::string input_file, vector<set<unsigned int>> &ns)
         cout<<"load graph file failed "<<endl;
         exit(-1);
     }
-    unsigned int V;
+    unsigned int V, s, t;
 
     infile>>V;
     // string line;
