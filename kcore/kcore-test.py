f45bdc7eff8e2c681343804e0f55e114cdbc2d9d
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-11 17:56:40 +0300

    ************ Working fine except block synch ***************

--- kcore/kcore-test.py
+++ kcore/kcore-test.py
@@ -3,7 +3,7 @@ dataset = "gowalla.g"
 G = nx.read_adjlist("./data_set/data/ours_format/" + dataset)
 nxkcore = nx.core_number(G)
 d = {}
-with open("../data_set/data/ours_format/" + dataset + "-kcore.txt") as f:
+with open("./data_set/data/ours_format/" + dataset + "-kcore.txt") as f:
     d[f.readline().strip()] = 0
     for line in f:
         (key, val) = line.split()
