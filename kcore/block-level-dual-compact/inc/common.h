17bfb702bc73ac74d92758d7b0811d5efe1da6b8
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-11 14:51:35 +0300

    atmoic checking

--- kcore/block-level-dual-compact/inc/common.h
+++ kcore/block-level-dual-compact/inc/common.h
@@ -1,13 +1,13 @@
 
 #ifndef CUTS_COMMON_H
 #define CUTS_COMMON_H
-#define BLK_NUMS 10
+#define BLK_NUMS 65
 #define BLK_DIM 1024
 #define WARPS_EACH_BLK (BLK_DIM/32)
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
 #define MAX_NV 6000
 #define N_THREADS (BLK_DIM*BLK_NUMS)
-#define GLBUFFER_SIZE 10000000
+#define GLBUFFER_SIZE 1000000
 #define THID threadIdx.x
 #define WARP_SIZE 32
 #define UINT unsigned int
