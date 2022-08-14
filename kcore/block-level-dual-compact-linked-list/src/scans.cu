b345a2d10dd72a7501a9be0635e360a429161e36
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-14 18:28:12 +0300

    linked list testing

--- kcore/block-level-dual-compact-linked-list/src/scans.cu
+++ kcore/block-level-dual-compact-linked-list/src/scans.cu
@@ -145,7 +145,7 @@ __device__ void compactWarp(unsigned int* temp, unsigned int* addresses, unsigne
         int nv = addresses[lane_id]+predicate[lane_id];
         bTail = nv>0? atomicAdd(bufTailPtr, nv) : 0;
         if(allocationRequired(tail[0], bTail+nv)){ // adding nv since bTail is old value of bufTail
-            atomicCAS((unsigned int*)&lock, 2, 0); // resets the lock in case a memory was allocated before
+            // atomicCAS((unsigned int*)&lock, 2, 0); // resets the lock in case a memory was allocated before
             // __threadfence_block();
             printf("Req %d", THID);
             allocateMemoryMutex(tail, head, lock);
