be3d1e07388e62f20f69b6dd15614e6e0882c740
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-14 18:12:35 +0300

    linked list testing

--- kcore/block-level-dual-compact-linked-list/inc/common.h
+++ kcore/block-level-dual-compact-linked-list/inc/common.h
@@ -1,7 +1,7 @@
 
 #ifndef CUTS_COMMON_H
 #define CUTS_COMMON_H
-#define BLK_NUMS 28
+#define BLK_NUMS 1
 #define BLK_DIM 1024
 #define WARPS_EACH_BLK (BLK_DIM/32)
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
