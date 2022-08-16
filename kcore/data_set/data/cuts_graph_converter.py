af076b295f721743dfec1ae052c9e395049b820f
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-17 00:16:45 +0300

    checking to improve performance

--- kcore/data_set/data/cuts_graph_converter.py
+++ kcore/data_set/data/cuts_graph_converter.py
@@ -3,29 +3,30 @@ import os
 import time
 import sys
 def convert_format(arg):
-    snap_graphs = arg[1]
-    cuts_graphs = snap_graphs.split('.')[0] + ".g"
+    in_file = arg[1]
+    graph = snap_graphs.split('.')[0] + ".g"
     comment_ch = '#@%!'
     st = time.time()
-    for i, graph in enumerate(snap_graphs):
-        lines = []
-        out_lines = []
-        max_node = 0
+    lines = []
+    out_lines = []
+    max_node = 0
 
-        writter = open(cuts_graphs[i], 'w')
-        with codecs.open(graph,'r','utf-8') as reader:
-            lines = reader.readlines()
-        for line in lines:
-            if line[0] in comment_ch:
-                continue
-            s, e = line.split()
-            if s == e:
-                continue
-            out_lines.append(line)
-            max_node = max(max_node, int(s), int(e))
-        writter.write(str(max_node+1)+'\n')
-        writter.writelines(out_lines)
-        writter.close()
+    with open(in_file,'r') as reader:
+        lines = reader.readlines()
+        
+    for line in lines:
+        if line[0] in comment_ch:
+            continue
+        s, e = line.split()
+        if s == e:
+            continue
+        out_lines.append(line)
+        max_node = max(max_node, int(s), int(e))
+
+    with open(graph, 'w') as writer:
+        writer.write(str(max_node+1)+'\n')
+        writer.writelines(out_lines)
+        writer.close()
     print("Elapsed: ", time.time()-st)
 
 if __name__ == "__main__":
