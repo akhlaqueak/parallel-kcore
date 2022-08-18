f6db28c8fd4f5528ffa1400fdbad2cb34b426e75
Akhlaque Ahmad <akhlaque.ak@gmail.com>
2022-08-19 00:46:37 +0300

    simplified

--- kcore-simplified/block-level-compact/src/device_funcs.cu
+++ kcore-simplified/block-level-compact/src/device_funcs.cu
@@ -96,7 +96,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
     glBuffer = NULL;
     base = 0;
     lock = 0;
-    glBuffer = glBuffers + blockIdx.x*GLBUFFER_SIZE; 
+    glBuffer = glBuffers + blockIdx.x * GLBUFFER_SIZE; 
 
     assert(glBuffer!=NULL);
     __syncthreads();
