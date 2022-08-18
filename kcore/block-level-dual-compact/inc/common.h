7e9baabec7746cd9d93754632a517fb98f2f49a9
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-18 19:31:53 +0300

    added separate kernel

--- kcore/block-level-dual-compact/inc/common.h
+++ kcore/block-level-dual-compact/inc/common.h
@@ -5,7 +5,7 @@
 #define BLK_DIM 512
 #define WARPS_EACH_BLK (BLK_DIM/32)
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
-#define MAX_NV 12000
+#define MAX_NV 11000
 #define N_THREADS (BLK_DIM*BLK_NUMS)
 #define GLBUFFER_SIZE 1000000
 #define THID threadIdx.x
