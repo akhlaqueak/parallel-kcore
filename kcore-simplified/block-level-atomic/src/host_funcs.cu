7fb9ed711d2fc1a31194c17d13d37296c4805c1b
Akhlaque Ahmad <akhlaque.ak@gmail.com>
2022-08-18 23:15:14 +0300

    simplified

--- kcore-simplified/block-level-atomic/src/host_funcs.cu
+++ kcore-simplified/block-level-atomic/src/host_funcs.cu
@@ -44,7 +44,7 @@ void find_kcore(string data_file,bool write_to_disk){
     cudaMallocManaged(&global_count,sizeof(unsigned int));
     cudaMallocManaged(&blockCounter,sizeof(unsigned int));
     
-    chkerr(cudaMalloc(&glBuffers,sizeof(unsigned int)*BLK_NUMS*GLBUFFER_SIZE ULL));
+    chkerr(cudaMalloc(&glBuffers,sizeof(unsigned int)*BLK_NUMS*GLBUFFER_SIZE));
     cout<<"allocations are requested for: "<< sizeof(unsigned int)*BLK_NUMS*GLBUFFER_SIZE ;
     cudaMemset(global_count,0,sizeof(unsigned int));
 
