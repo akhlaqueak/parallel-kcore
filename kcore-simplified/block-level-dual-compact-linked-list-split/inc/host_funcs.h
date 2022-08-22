f05c577a5db8d4c25bdb0056547bbbf4103163fb
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-22 17:25:55 +0300

    folder names changed

--- kcore-simplified/block-level-dual-compact-linked-list-split/inc/host_funcs.h
+++
@@ -1,9 +0,0 @@
-
-#ifndef CUTS_HOST_FUNCS_H
-#define CUTS_HOST_FUNCS_H
-#include "./gpu_memory_allocation.h"
-#include "./device_funcs.h"
-void find_kcore(string data_file,bool write_to_disk);
-void find_kcore_CPU(string data_file,bool write_to_disk);
-
-#endif //CUTS_HOST_FUNCS_H
--- kcore/warp-level-atomic/build/src/util.cpp.d
+++
@@ -1,9 +0,0 @@
-build/./src/util.cpp.o: src/util.cpp src/../inc/util.h \
- src/../inc/./common.h \
- /cm/shared/apps/cuda11.4/toolkit/11.4.2/targets/x86_64-linux/include/cuda.h
-
-src/../inc/util.h:
-
-src/../inc/./common.h:
-
-/cm/shared/apps/cuda11.4/toolkit/11.4.2/targets/x86_64-linux/include/cuda.h:
