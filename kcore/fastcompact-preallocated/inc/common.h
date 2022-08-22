9190b254e166d2184d9c63d36fe21febbeed5565
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-22 14:06:40 +0300

    save everthing

---
+++ kcore/fastcompact-preallocated/inc/common.h
@@ -0,0 +1,41 @@
+
+#ifndef CUTS_COMMON_H
+#define CUTS_COMMON_H
+#define BLK_NUMS 56
+#define BLK_DIM 512
+#define WARPS_EACH_BLK (BLK_DIM/32)
+#define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
+#define MAX_NV 10000
+#define N_THREADS (BLK_DIM*BLK_NUMS)
+#define GLBUFFER_SIZE 1000000
+#define THID threadIdx.x
+#define WARP_SIZE 32
+#include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <fstream>
+#include <map>
+#include <utility>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <algorithm>
+#include <unordered_map>
+#include <stack>
+#include <deque>
+#include <random>
+#include <cuda.h>
+#include <bits/stdc++.h>
+#include "omp.h"
+
+using namespace std;
+
+typedef struct G_pointers {
+    unsigned int* neighbors;
+    unsigned int* neighbors_offset;
+    unsigned int* degrees;
+    unsigned int V;
+} G_pointers;//graph related
+
+#endif //CUTS_COMMON_H
