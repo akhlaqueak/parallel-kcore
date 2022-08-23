198369b7aa8d818aded557bf99206c1c619d0444
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-23 16:27:05 +0300

    folder names changed

--- kcore/compact-linked-list-noshared/inc/graph.h
+++ kcore/compact-linked-list-noshared/inc/graph.h
@@ -12,8 +12,8 @@ public:
     unsigned int * neighbors_offset;
     unsigned int * degrees;
     Graph(std::string input_file);
-    readSerialized(string input_file);
-    writeSerialized(string input_file);
+    bool readSerialized(string input_file);
+    void writeSerialized(string input_file);
     ~Graph();
 };
 #endif //CUTS_GRAPH_H
