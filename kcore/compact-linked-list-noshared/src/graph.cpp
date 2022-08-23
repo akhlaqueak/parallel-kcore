f18a530cacbf785edf893f88af21071e80c193fd
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-23 16:25:07 +0300

    folder names changed

--- kcore/compact-linked-list-noshared/src/graph.cpp
+++ kcore/compact-linked-list-noshared/src/graph.cpp
@@ -1,9 +1,27 @@
 
 #include "../inc/graph.h"
+bool Graph::readSerialized(string input_file){
+    ifstream file;
+    if(file.open(DS_LOC + "serialized-" + input_file, 'r')){
+        file>>V;
+        file>>E;
+        degrees = new unsigned int[V];
+        neighbors_offset = new unsigned int[V+1];
+        neighbors = new unsigned int[E];
+        for(int i=0;i<V;i++)
+            file>>degrees[i];
+        for(int i=0;i<V+1;i++)
+            file>>neighbors_offset[i];
+        for(int i=0;i<E;i++)
+            file>>neighbors[i];
+        return true;
+    }
+    file.close();
 
+    return false;
+}
 
-Graph::Graph(std::string input_file){
-    
+void Graph::writeSerialized(string input_file){
     vector< set<unsigned int> > ns;
     V = file_reader(input_file, ns);
     degrees = new unsigned int[V];
@@ -28,7 +46,24 @@ Graph::Graph(std::string input_file){
         for(int j=neighbors_offset[i]; j < neighbors_offset[i+1]; j++, it++)
             neighbors[j] = *it;
     }
+    ofstream file;
+    if(file.open(DS_LOC + "serialized-" + input_file, 'w')){
+        file<<V;
+        file<<E;
+        for(int i=0;i<V;i++)
+            file<<degrees[i]<<' ';
+        for(int i=0;i<V+1;i++)
+            file<<neighbors_offset[i]<<' ';
+        for(int i=0;i<E;i++)
+            file<<neighbors[i]<<' ';
+    }
+    file.close();
+}
 
+Graph::Graph(std::string input_file){
+    if(readSerialized(input_file)) return;
+    
+    writeSerialized(input_file);
 }
 
 Graph::~Graph(){
