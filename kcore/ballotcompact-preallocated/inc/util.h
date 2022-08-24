eb70878669e9d03a0a499c2659a013ffc6a655c6
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-24 11:56:09 +0300

    folder names changed

---
+++ kcore/ballotcompact-preallocated/inc/util.h
@@ -0,0 +1,9 @@
+
+
+#ifndef CUTS_UTIL_H
+#define CUTS_UTIL_H
+#include "./common.h"
+unsigned int file_reader(std::string input_file, vector<set<unsigned int>> &ns);
+
+void write_kcore_to_disk(unsigned int*, unsigned long long int, std::string file);
+#endif //CUTS_UTIL_H
