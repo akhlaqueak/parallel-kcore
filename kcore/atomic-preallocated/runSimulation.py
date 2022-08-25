c1a20fc0be65d77b4062b174869b7a7567a6ffbd
Akhlaque <akhlaque.ak@gmail.com>
2022-08-25 17:55:36 +0300

    hello

--- kcore/atomic-preallocated/runSimulation.py
+++ kcore/atomic-preallocated/runSimulation.py
@@ -31,7 +31,7 @@ def parse(arg):
 def runSim(datasets):
     results = []
     for ds in datasets:
-        output = sp.run("./kcore", ds)
+        output = sp.run(["./kcore", ds])
         time = parseResult(output.stdout)
         results.append(tuple(ds, time))
     return results
