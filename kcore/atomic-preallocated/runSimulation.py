5e0c413d8026b36aa97f83a6081951bf79eb45af
Akhlaque <akhlaque.ak@gmail.com>
2022-08-25 18:00:44 +0300

    hello

--- kcore/atomic-preallocated/runSimulation.py
+++ kcore/atomic-preallocated/runSimulation.py
@@ -8,12 +8,15 @@ def parseResult(output):
     line = [x for x in output.split("\n") if "Elapsed Time" in x]
     return line[0].split(":")[1]
 
-def parse(arg):
+def parse(args):
     datasets = ("Enron.g", "wikipedia-link-de.g", "trackers.g", "soc-Journal.g", "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g", "wikiTalk.g")
-    ds = []
-    if arg == "":
-        ds.append("Enron.g")
-    elif arg.isnumeric():
+    arg = "" if len(args)<1 else args[1]
+
+    if(len(args)<1):
+        arg = ""
+    # if arg == "":
+    #     ds.append("Enron.g")
+    if arg.isnumeric():
         ind = int(arg)
         if(ind<len(datasets)):
             ds.append(datasets[int(arg)])
@@ -40,7 +43,7 @@ def runSim(datasets):
 if __name__ == "__main__":
     sp.run(["git", "pull"])
     sp.run(["make"])
-    datasets = parse(sys.argv[1])
+    datasets = parse(sys.argv)
     results = runSim(datasets)
     for ds, time in results.items():
         print(ds, time)
