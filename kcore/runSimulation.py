db49b83babe80d48f998dc9c78904da54d9057a4
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-26 17:25:45 +0300

    $1

--- kcore/runSimulation.py
+++ kcore/runSimulation.py
@@ -5,7 +5,7 @@ import json
 import networkx as nx
 from subprocess import PIPE
 
-FOLDERS = ["atomic", "fastcompact", "fastcompact-warponly", "ballotcompact", "ballotcompact-warponly"]
+FOLDERS = ["ballotcompact-linkedlist"]
 OUTPUT = "../output/"
 DATASET = "../data_set/data/ours_format/"
 VERIFY = False
@@ -38,7 +38,7 @@ def parseResult(output):
     line = [x for x in output.split("\n") if "Elapsed Time" in x]
     return line[0].split(":")[1]
 
-def parse(args):
+def parseDataSet(args):
     datasets = ("Enron.g", "wikipedia-link-de.g", "trackers.g", "soc-Journal.g", "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g", "wikiTalk.g")
     ds = []
     arg = "" if len(args)<2 else args[1]
@@ -70,10 +70,32 @@ def runSim(datasets):
         print("Completed")
     return results
 
+def parseFolder(args):
+    folders = ["atomic", "fastcompact", "fastcompact-warponly", "ballotcompact", "ballotcompact-warponly", "ballotcompact-linkedlist"]
+    folder = []
+    arg = "" if len(args)<3 else args[2]
 
+    if arg == "":
+        folder.append(folders[0])
+    elif arg.isnumeric():
+        ind = int(arg)
+        if(ind<len(folders)):
+            folder.append(folders[int(arg)])
+        else:
+            print("Please provide valid folder: ", [(x, y) for x, y in enumerate(folders)])
+    elif arg == "all":
+        folder = folders
+    elif arg in folders:
+        folder.append(arg) 
+    else:
+        print("Please provide valid folder: ", [(x, y) for x, y in enumerate(folders)])
+        exit(0)
+    return folder
 if __name__ == "__main__":
-    datasets = parse(sys.argv)
-    for folder in FOLDERS:
+    datasets = parseDataSet(sys.argv)
+    folders = parseFolder(sys.argv)
+
+    for folder in folders:
         os.chdir(folder)
         sp.run(["make"])
         print("Executing in ", folder)
