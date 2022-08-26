db8a7f2ca95b78e07059885a5ef90f9897c4fe3c
Akhlaque <akhlaque.ak@gmail.com>
2022-08-26 13:17:19 +0300

    woner

--- kcore/ballotcompact/inc/common.h
+++ kcore/ballotcompact/inc/common.h
@@ -6,10 +6,15 @@
 #define WARPS_EACH_BLK (BLK_DIM/32)
 #define WORK_UNITS (BLK_NUMS*WARPS_EACH_BLK)
 #define MAX_NV 10000
+#define BUFF_SIZE 20000
 #define N_THREADS (BLK_DIM*BLK_NUMS)
 #define GLBUFFER_SIZE 1000000
 #define THID threadIdx.x
 #define WARP_SIZE 32
+#define UINT unsigned int
+#define DS_LOC string("../data_set/data/ours_format/")
+#define OUTPUT_LOC string("../output/")
+
 #include <iostream>
 #include <vector>
 #include <set>
@@ -37,5 +42,13 @@ typedef struct G_pointers {
     unsigned int* degrees;
     unsigned int V;
 } G_pointers;//graph related
+
+typedef struct Node {
+    unsigned int data[BUFF_SIZE];
+    unsigned int limit;
+    Node* next;
+    Node* prev;
+} Node;
+
 
 #endif //CUTS_COMMON_H
