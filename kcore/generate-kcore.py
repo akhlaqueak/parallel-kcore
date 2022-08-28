f46d7f1d0357ecc893b6e79f18da0c017598d50e
akhlaqueak <akhlaque.ak@gmail.com>
2022-08-28 15:22:58 +0300

    $1

--- kcore/generate-kcore.py
+++ kcore/generate-kcore.py
@@ -1,16 +1,27 @@
 import networkx as nx
 import json
 import time
-datasets = ["soc-Journal.g", "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g"]
-# ds = ['33.g']
-
+# datasets = ["soc-Journal.g", "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g"]
+# datasets = ["wikipedia-link-de.g"]
+datasets = ['patentcite.g']
 for ds in datasets:
-    G = nx.read_adjlist("./data_set/data/ours_format/" + ds)
     tick = time.time()
-    nxkcore = nx.core_number(G)
+    G = nx.read_edgelist("./data_set/data/ours_format/" + ds)
     tock = time.time()
-    print(ds, tock-tick)
-    json.dump(nxkcore, open("./output/gennx-kcore-" + ds, 'w'))
+    print("Time in edgelist", tock-tick)
+    
+    tick = time.time()
+    G = nx.read_adjlist("./data_set/data/ours_format/" + ds)
+    tock = time.time()
+    print("Time in edgelist", tock-tick)
+    
+    
+    # tick = time.time()
+
+    # nxkcore = nx.core_number(G)
+    # tock = time.time()
+    # print(ds, tock-tick)
+    # json.dump(nxkcore, open("./output/gennx-kcore-" + ds, 'w'))
     # with open("output/" + dataset + "-nx-kcore", 'w') as f: 
     #     f.write(json.dumps(nxkcore))
         # for key, value in details.items(): 
