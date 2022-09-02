862dc0837852a779dc2335a39be38d8b2fe1d461
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-02 15:35:16 +0300

    $1

--- kcore/atomic/src/device_funcs.cu
+++ kcore/atomic/src/device_funcs.cu
@@ -101,7 +101,7 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
 
 
         while(true){
-            // __syncwarp();
+            __syncwarp();
 
             if(start >= end) break;
 
