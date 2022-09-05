aaf96e9e41538ddadb688b720a209b9371d4fd6b
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-05 16:38:03 +0300

    atomic linked list

--- kcore/runSimulation.py
+++ kcore/runSimulation.py
@@ -7,7 +7,7 @@ import networkx as nx
 from subprocess import PIPE
 folders = ["atomic", "fastcompact", "fastcompact-warponly", "ballotcompact", \
         "ballotcompact-warponly", "ballotcompact-linkedlist", "ballotcompact-warponly-sep-kernels", "cpu", 
-        "atomic-linked-list-separate-kernels"]
+        "atomic-linked-list-separate-kernels", "compact-linked-list-separate-kernels"]
 
 datasets = ["Enron.g", "wikipedia-link-de.g", "trackers.g", "soc-Journal.g", \
     "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g", "wikiTalk.g", "twitter_mpi.g"\
