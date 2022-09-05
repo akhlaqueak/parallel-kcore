97b88d0a0f97d554f61716d85ade00914a75091c
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-05 15:31:48 +0300

    atomic linked list

--- kcore/atomic-linked-list-separate-kernels/src/device_funcs.cu
+++ kcore/atomic-linked-list-separate-kernels/src/device_funcs.cu
@@ -63,7 +63,6 @@ __global__ void PKC(G_pointers d_p, unsigned int *global_count, int level, int V
         base = 0;
         lock = 0;
     }
-    predicate[THID] = 0;
 
     // if(level == 1 && THID == 0) printf("%d ", bufTail);
 
