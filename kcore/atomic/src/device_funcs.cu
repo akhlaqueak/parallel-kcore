d3918f8938f6480662baf4844a5d438fc1e449f7
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-07 17:34:15 +0300

    atomic linked list

--- kcore/atomic/src/device_funcs.cu
+++ kcore/atomic/src/device_funcs.cu
@@ -28,7 +28,7 @@ __device__ void syncBlocks(unsigned int* blockCounter){
 
     const unsigned long long int SollMask = (1 << BLK_NUMS) - 1;
     if (THID == 0) {
-        while ((atomicOr(&ct, 1ULL << blockIdx.x)) != SollMask) { printf("%X,%u ", SollMask, blockIdx.x); }
+        while ((atomicOr(&ct, 1ULL << blockIdx.x)) != SollMask) { printf("%u ", blockIdx.x); }
     }
 
 
