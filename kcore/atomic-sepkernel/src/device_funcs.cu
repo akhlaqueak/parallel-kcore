f9048668c9b719d3cf344b65d91c73a950da336f
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-11 14:27:04 +0300

    atomic linked list

--- kcore/atomic-sepkernel/src/device_funcs.cu
+++ kcore/atomic-sepkernel/src/device_funcs.cu
@@ -36,7 +36,7 @@ __global__ void processNodes(G_pointers d_p, int level, int V,
                     unsigned int* bufTails, unsigned int* glBuffers, 
                     unsigned int *global_count){
 
-    __shared__ unsigned int* shBuffer;
+    __shared__ unsigned int shBuffer[MAX_NV];
     __shared__ unsigned int bufTail;
     __shared__ unsigned int* glBuffer;
     __shared__ unsigned int base;
