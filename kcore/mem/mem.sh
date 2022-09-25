3e9459c7bcc3c0e746002ebba34bc3c014619163
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-25 23:44:32 +0300

    trying atomic

--- kcore/mem/mem.sh
+++ kcore/mem/mem.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-rm $1
+rm a.txt
 while true;
 do
         # free: Display memory details. -m to show values in mibibytes.
         # grep: search for 'Mem'
         # awk: print the value of the third column "used memory"
-        nvidia-smi --query-gpu=memory.used --format=csv,nounits,noheader >> $1
+        nvidia-smi --query-gpu=memory.used --format=csv,nounits,noheader >> a.txt
         sleep 0.001; #second
 #       echo "hi"
 done
