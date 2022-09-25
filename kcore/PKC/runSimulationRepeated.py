9624b069ac45f31494cf63be26bc5c9e341c13a7
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-25 18:21:23 +0300

    PKC added

--- kcore/PKC/runSimulationRepeated.py
+++ kcore/PKC/runSimulationRepeated.py
@@ -88,7 +88,7 @@ def repeatedSimulations(datasets):
             execTime[ds].append(exec[ds])
     print("###---------------###")
     print("###- Execution Time -###")
-    types = execTime[datasets[0]]
+    types = execTime[datasets[0]][0].keys()
     print("datasets ", types)
     for ds in datasets:
         print(ds, end=" ")
