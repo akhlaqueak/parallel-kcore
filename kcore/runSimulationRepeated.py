9da7a0333f740d37a97377a238c9930e28e1ebc8
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-26 12:27:19 +0300

    trying atomic

--- kcore/runSimulationRepeated.py
+++ kcore/runSimulationRepeated.py
@@ -15,31 +15,31 @@ folders = ["atomic", "atomic-sepkernel", "atomic-sepkernel-prefetched", "ballotc
 #     "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g", "wikiTalk.g", "twitter_mpi.g"\
 #     ]
 # datasets = ['amazon0601.txt',   'cit-Patents.txt',     'in-2004.txt',         'patentcite.txt',        'uk-2002.txt',  'web-BerkStan.txt',  'wiki-Talk.txt', 'arabic-2005.txt',  'dblp-author.txt',     'indochina-2004.txt',  'uk-2005.txt',  'web-Google.txt', 'as-skitter.txt',   'hollywood-2009.txt', 'it-2004.txt',         'soc-LiveJournal1.txt',  'wb-edu.txt',   'webbase-2001.txt']
-datasets = ['amazon0601.txt',   'cit-Patents.txt',     'in-2004.txt',         'patentcite.txt']
+# datasets = ['amazon0601.txt',   'cit-Patents.txt',     'in-2004.txt',         'patentcite.txt']
 
-# datasets = [
-# 'amazon0601.txt', \
-# 'arabic-2005.txt', \
-# 'as-Skitter.txt', \
-# 'dblp-author.txt', \
-# 'hollywood-2009.txt', \
-# 'in-2004.txt', \
-# 'indochina-2004.txt', \
-# 'it-2004.txt', \
-# 'patentcite.txt', \
+datasets = [
+'amazon0601.txt', \
+'arabic-2005.txt', \
+'as-skitter.txt', \
+'dblp-author.txt', \
+'hollywood-2009.txt', \
+'in-2004.txt', \
+'indochina-2004.txt', \
+'it-2004.txt', \
+'patentcite.txt', \
 # 'sk-2005.txt', \
-# 'soc-LiveJournal1.txt', \
-# 'soc-Pokec.txt', \
-# 'trackers.txt', \
-# 'uk-2002.txt', \
-# 'uk-2005.txt', \
-# 'wb-edu.txt', \
-# 'webbase-2001.txt', \
-# 'web-BerkStan.txt', \
-# 'web-Google.txt', \
-# 'wikipedia-link-de.txt', \
-# 'wiki-Talk.txt', \
-# ]
+'soc-LiveJournal1.txt', \
+'soc-Pokec.txt', \
+'trackers.txt', \
+'uk-2002.txt', \
+'uk-2005.txt', \
+'wb-edu.txt', \
+'webbase-2001.txt', \
+'web-BerkStan.txt', \
+'web-Google.txt', \
+'wikipedia-link-de.txt', \
+'wiki-Talk.txt', \
+]
 OUTPUT = "../output/"
 DATASET = "../data_set/data/"
 VERIFY = False
