eda25ee96b4bf60deb6df016b95f2cda5136e974
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-28 09:34:13 +0300

    PKC added

--- kcore/networkx/generate-kcore.py
+++ kcore/networkx/generate-kcore.py
@@ -2,15 +2,25 @@ import networkx as nx
 import json
 import time
 # datasets = ["soc-Journal.g", "dblp-author.g", "patentcite.g", "soc-pokec-relationships.g"]
-datasets = ['web-BerkStan.txt',\
-'soc-LiveJournal1.txt',\
-'wb-edu.txt',\
-'hollywood-2009.txt',\
-'web-Google.txt',\
-'wiki-Talk.txt',\
+datasets = ['amazon0601.txt', \
+'web-BerkStan.txt', \
+'web-Google.txt', \
+'hollywood-2009.txt', \
+'in-2004.txt', \
+'as-Skitter .txt', \
+'wiki-Talk.txt', \
+'wikipedia-link-de.txt', \
+'patentcite.txt', \
+'soc-LiveJournal1.txt', \
+'dblp-author.txt', \
+'indochina-2004.txt', \
+'wb-edu.txt', \
+'uk-2002.txt', \
+'arabic-2005.txt', \
 ]
 # datasets = ['patentcite.g']
 for ds in datasets:
+    print("Starting time", ds, time.time())
     tick = time.time()
     G = nx.read_edgelist("./data_set/data/" + ds)
     tock = time.time()
