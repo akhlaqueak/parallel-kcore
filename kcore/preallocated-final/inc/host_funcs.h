20f472a2e4e8f86667dcc9ae5cde100677a175d2
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-29 22:37:21 +0300

    PKC added

--- kcore/preallocated-final/inc/host_funcs.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef CUTS_HOST_FUNCS_H
-#define CUTS_HOST_FUNCS_H
-#include "./gpu_memory_allocation.h"
-#include "./device_funcs.h"
-int find_kcore(Graph, bool write_to_disk);
-
-#endif //CUTS_HOST_FUNCS_H
