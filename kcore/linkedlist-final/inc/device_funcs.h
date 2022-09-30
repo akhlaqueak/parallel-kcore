c9eebed0828275107d5b871cdbd15099e190862e
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-30 13:59:46 +0300

    PKC added

--- kcore/linkedlist-final/inc/device_funcs.h
+++ kcore/linkedlist-final/inc/device_funcs.h
@@ -4,11 +4,10 @@
 #define CUTS_DEVICE_FUNCS_H
 #include "./common.h"
 
-__global__ void selectNodesAtLevel(unsigned int *degrees, unsigned int level, unsigned int V, 
-                 unsigned int* bufTails, unsigned int* glBuffers);
-
-
-__global__ void processNodes(G_pointers d_p, int level, int V, 
-                    unsigned int* bufTails, unsigned int* glBuffers, 
-                    unsigned int *global_count);
+__global__ void selectNodesAtLevel(unsigned int* degrees, unsigned int level, 
+                        unsigned int V, unsigned int* bufTails,
+                        Node** heads, Node** tails);
+                        
+__global__ void processNodes(G_pointers d_p, int level, int V, unsigned int* bufTails, 
+    unsigned int* global_count, Node** heads, Node** tails);
 #endif //CUTS_DEVICE_FUNCS_H
