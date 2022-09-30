bea1de0d1e7dbc6ed668bd9f7b103b89f47c613a
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-30 11:58:25 +0300

    PKC added

--- kcore/ballotcompact-final/inc/common.h
+++ kcore/ballotcompact-final/inc/common.h
@@ -8,7 +8,7 @@
 #define MAX_NV 10000
 #define BUFF_SIZE 20000
 #define N_THREADS (BLK_DIM*BLK_NUMS)
-#define GLBUFFER_SIZE 1000000
+#define GLBUFFER_SIZE 100000
 #define THID threadIdx.x
 #define WARP_SIZE 32
 #define UINT unsigned int
