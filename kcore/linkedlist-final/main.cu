12af0cd3f8ba8ae1e115a5b50082c3fe409459b9
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-30 14:17:50 +0300

    PKC added

--- kcore/linkedlist-final/main.cu
+++ kcore/linkedlist-final/main.cu
@@ -26,10 +26,12 @@ int find_kcore(Graph &data_graph,bool write_to_disk){
     cudaMallocManaged(&bufTails,sizeof(unsigned int)*BLK_NUMS);
     cudaMallocManaged(&heads,sizeof(Node*)*BLK_NUMS);
     cudaMallocManaged(&tails,sizeof(Node*)*BLK_NUMS);
+    cout<<"allocations done"<<endl;
 
     chkerr(cudaMalloc(&global_count, sizeof(unsigned int)));
     // chkerr(cudaMalloc(&bufTails, sizeof(unsigned int)*BLK_NUMS));
     cudaMemset(global_count, 0, sizeof(unsigned int));
+    cout<<"sl done"<<endl;
        
     size_t limit = 10*1024*1024*1024ULL; //10GB
     chkerr(cudaDeviceSetLimit(cudaLimitMallocHeapSize, limit));
