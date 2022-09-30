400f3d09b891c4720dba97e2f5399bb549e8e76f
akhlaqueak <akhlaque.ak@gmail.com>
2022-09-30 13:57:06 +0300

    PKC added

---
+++ kcore/linkedlist-final/main.cu
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+
+#include "../inc/gpu_memory_allocation.h"
+#include "../inc/device_funcs.h"
+
+int find_kcore(Graph &data_graph,bool write_to_disk){
+
+    G_pointers data_pointers;
+
+
+    cout<<"Device Copy Started "<<data_graph.V<<data_graph.E<<endl;
+    malloc_graph_gpu_memory(data_graph, data_pointers);
+    cout<<"Device Copy Done"<<endl;
+
+    unsigned int level = 0;
+    unsigned int count = 0;
+    unsigned int* global_count  = NULL;
+    unsigned int* bufTails  = NULL;
+    Node** heads;
+    Node** tails;
+
+
+    cudaMallocManaged(&bufTails,sizeof(unsigned int)*BLK_NUMS);
+    cudaMallocManaged(&heads,sizeof(Node*)*BLK_NUMS);
+    cudaMallocManaged(&tails,sizeof(Node*)*BLK_NUMS);
+
+    chkerr(cudaMalloc(&global_count, sizeof(unsigned int)));
+    // chkerr(cudaMalloc(&bufTails, sizeof(unsigned int)*BLK_NUMS));
+    cudaMemset(global_count, 0, sizeof(unsigned int));
+       
+    size_t limit = 10*1024*1024*1024ULL; //10GB
+    chkerr(cudaDeviceSetLimit(cudaLimitMallocHeapSize, limit));
+
+	cout<<"K-core Computation Started"<<endl;
+
+    auto start = chrono::steady_clock::now();
+    while(count < data_graph.V){
+        for(int i=0;i<BLK_NUMS;i++){
+            heads[i] = NULL;
+            tails[i] = NULL;
+            bufTails[i] = 0;
+        }
+        selectNodesAtLevel<<<BLK_NUMS, BLK_DIM>>>(data_pointers.degrees, level, 
+                        data_graph.V, bufTails, heads, tails);
+
+        processNodes<<<BLK_NUMS, BLK_DIM>>>(data_pointers, level, data_graph.V, 
+                        bufTails, global_count, heads, tails);
+
+        chkerr(cudaMemcpy(&count, global_count, sizeof(unsigned int), cudaMemcpyDeviceToHost));    
+        cout<<"*********Completed level: "<<level<<", global_count: "<<count<<" *********"<<endl;
+        level++;
+    }
+	cout<<"K-core Computation Done"<<endl;
+
+    auto end = chrono::steady_clock::now();
+    
+    
+    // cout << "Elapsed Time: "
+    // << chrono::duration_cast<chrono::milliseconds>(end - start).count() << endl;
+    // cout <<"MaxK: "<<level-1<<endl;
+    
+    
+	// get_results_from_gpu(data_graph, data_pointers);
+    
+    free_graph_gpu_memory(data_pointers);
+    // if(write_to_disk){
+    //     cout<<"Writing kcore to disk started... "<<endl;
+    //     data_graph.writeKCoreToDisk(data_file);
+    //     cout<<"Writing kcore to disk completed... "<<endl;
+    // }
+
+    return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+
+}
+
+
+int main(int argc, char *argv[]){
+    if (argc < 2) {
+        cout<<"Please provide data file"<<endl;
+        exit(-1);
+    }
+    std::string data_file = argv[1];
+    bool write_to_disk = false;
+
+    cout<<"Loading Started"<<endl;    
+    Graph data_graph(data_file);
+    cout<<"Loading Done"<<endl;
+    
+    vector<int> et;
+    for(int i=0;i<REP; i++){
+        cout<<"Running iteration: "<<i+1<<endl;
+        int t = find_kcore(data_graph, write_to_disk);
+        et.push_back(t);
+    }
+    cout << data_file << " Elapsed Time: ";
+
+    for(auto t: et)
+        cout<<t<<" ";
+    cout<<(double)accumulate(et.begin(), et.end(), 0)/et.size();
+    cout<<endl;
+    return 0;
+}
---
+++ kcore/linkedlist-final/src/device_funcs.cu
@@ -0,0 +1,141 @@
+
+#include "../inc/device_funcs.h"
+// including these cuda files as inc files, so that to create a single compilation unit
+
+#include "./buffer.cc"
+#include "./scans.cc"
+__global__ void selectNodesAtLevel(unsigned int* degrees, unsigned int level, 
+                        unsigned int V, unsigned int* bufTails,
+                        Node** heads, Node** tails){
+    __shared__ unsigned int bufTail;
+    __shared__ unsigned int temp[BLK_DIM]; 
+    __shared__ volatile unsigned int addresses[BLK_DIM];
+    __shared__ bool unsigned int predicate[BLK_DIM];
+    __shared__ Node** head, tail;
+
+    if(THID == 0){
+        head = heads + blockIdx.x;
+        tail = tails + blockIdx.x;
+        bufTail = 0;
+    }
+    __syncthreads();
+    
+    unsigned int glThreadIdx = blockIdx.x * BLK_DIM + THID; 
+
+    for(unsigned int base = 0; base < V; base += N_THREADS){
+        
+        unsigned int v = base + glThreadIdx; 
+
+        // all threads should get some value, if vertices are less than n_threads, rest of the threads get zero
+        predicate[THID] = (v<V)? (degrees[v] == level) : 0;
+        temp[THID] = v;
+
+        compactBlock(predicate, addresses, temp, tail, head, &bufTail);        
+        __syncthreads();
+            
+    }
+
+    if(THID == 0)
+        bufTails[blockIdx.x] = bufTail;
+}
+
+
+__global__ void processNodes(G_pointers d_p, int level, int V, unsigned int* bufTails, 
+    unsigned int* global_count, Node** heads, Node** tails){
+
+    
+    __shared__ Node** tail;
+    __shared__ Node** head;
+    __shared__ unsigned int bufTail;
+    __shared__ unsigned int base;
+    __shared__ bool predicate[BLK_DIM];
+    __shared__ unsigned int temp[BLK_DIM];
+    __shared__ volatile unsigned int addresses[BLK_DIM];
+    __shared__ volatile unsigned int lock;
+
+    unsigned int warp_id = THID / 32;
+    unsigned int lane_id = THID % 32;
+    unsigned int i;
+    if(THID==0){
+        tail = tails + blockIdx.x;
+        head = heads + blockIdx.x;
+        bufTail = 0;
+        base = 0;
+        lock = 0;
+    }
+    predicate[THID] = 0;
+    
+
+    // bufTail is being incremented within the loop, 
+    // warps should process all the nodes added during the execution of loop
+    // for that purpose base is introduced, is incremented whenever a warp takes a job.
+    
+
+    // bufTail = 10;
+    // for(unsigned int i = warp_id; i<bufTail ; i += WARPS_EACH_BLK){
+    // this for loop is a wrong choice, as many threads might exit from the loop checking the condition     
+    while(true){
+        __syncthreads(); //syncthreads must be executed by all the threads, so can't put after break or continue...
+        if(base == bufTail) break;
+        i = base + warp_id;
+        
+        if(THID==0 && head[0]!=NULL)
+            if(base >= head[0]->limit){
+                advanceNode(head);
+            }
+
+        if(THID == 0){
+            base += WARPS_EACH_BLK;
+            if(bufTail < base )
+                base = bufTail;
+        }
+        __syncthreads();
+        if(i >= bufTail) continue; // this warp won't have to do anything 
+
+        
+        unsigned int v = readFromBuffer(shBuffer, head, i);
+        unsigned int start = d_p.neighbors_offset[v];
+        unsigned int end = d_p.neighbors_offset[v+1];
+        // for(int j = start + lane_id; j<end ; j+=32){
+        // the for loop may leave some of the threads inactive in its last iteration
+        // following while loop will keep all threads active until the continue condition
+        while(true){
+            __syncwarp();
+
+            compactWarp(predicate, addresses, temp, 
+                        shBuffer, tail, head, &bufTail, &lock, total);
+            predicate[THID] = 0;
+            __syncwarp();
+
+            if(start >= end) break;
+
+            unsigned int j = start + lane_id;
+            start += WARP_SIZE;
+            if(j >= end) continue;
+
+            unsigned int u = d_p.neighbors[j];
+            if(*(d_p.degrees+u) > level){
+                unsigned int a = atomicSub(d_p.degrees+u, 1);
+                
+                if(a == level+1){
+                    temp[THID] = u;
+                    predicate[THID] = 1;
+                }
+
+                if(a <= level){
+                    // node degree became less than the level after decrementing... 
+                    atomicAdd(d_p.degrees+u, 1);
+                }
+                // __threadfence();
+            }
+        }        
+    }
+    
+    __syncthreads();
+
+    if(THID == 0 && bufTail!=0){
+        atomicAdd(global_count, bufTail); // atomic since contention among blocks
+        if(head[0]!=NULL) free(head[0]);
+    }
+
+}
