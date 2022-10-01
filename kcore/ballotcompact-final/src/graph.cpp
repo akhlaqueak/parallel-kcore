0a8ea6c1075003dc85928c046ffd3615fd0fb88a
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-01 13:47:39 +0300

    PKC added

--- kcore/ballotcompact-final/src/graph.cpp
+++ kcore/ballotcompact-final/src/graph.cpp
@@ -17,7 +17,7 @@ bool Graph::readSerialized(string input_file){
         for(int i=0;i<E;i++)
             file>>neighbors[i];
         file.close();
-        cout<<"MAXD: "<<*max_element(degrees, degrees+V);
+        cout<<input_file<<" MAXD: "<<*max_element(degrees, degrees+V);
         return true;
     }else{
         cout<<"readSerialized: File couldn't open"<<endl;
