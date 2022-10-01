69d301ef89dac6adebebc820526526f69f6ea52a
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-01 17:30:14 +0300

    PKC added

--- kcore/networkx/generate-kcore.py
+++ kcore/networkx/generate-kcore.py
@@ -3,30 +3,30 @@ import json
 import time
 
 datasets = [
-'amazon0601', \
-'web-Google', \
-'dblp-author', \
-'patentcite', \
-'web-BerkStan', \
-'wiki-Talk', \
-'as-skitter', \
-'in-2004', \
-'wb-edu', \
-'soc-LiveJournal1', \
-'com-orkut', \
-'hollywood-2009', \
-'uk-2002', \
-'wikipedia-link-de', \
-'uk-2005', \
-'indochina-2004', \
-'trackers', \
-'arabic-2005', \
-'webbase-2001', \
-'it-2004', \
+'amazon0601.txt', \
+'web-Google.txt', \
+'dblp-author.txt', \
+'patentcite.txt', \
+'web-BerkStan.txt', \
+'wiki-Talk.txt', \
+'as-skitter.txt', \
+'in-2004.txt', \
+'wb-edu.txt', \
+'soc-LiveJournal1.txt', \
+'com-orkut.txt', \
+'hollywood-2009.txt', \
+'uk-2002.txt', \
+'wikipedia-link-de.txt', \
+'uk-2005.txt', \
+'indochina-2004.txt', \
+'trackers.txt', \
+'arabic-2005.txt', \
+'webbase-2001.txt', \
+'it-2004.txt', \
 ]
 # datasets = ['patentcite.g']
 for ds in datasets:
-    # try:
+    try:
         now = time.strftime("%H:%M", time.localtime(time.time()))
         print("Starting time", ds, now)
         tick = time.time()
@@ -39,6 +39,7 @@ for ds in datasets:
         nxkcore = nx.core_number(G)
         tock = time.time()
         print("Excecution time: ", ds, tock-tick)
-    # except:
+    except Exception as e:
+        print(str(e))
         print("Failed... ", ds)
     # json.dump(nxkcore, open("./output/networkx-kcore-" + ds, 'w'))
