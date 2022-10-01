d2701a91915d9540026cd7b10100f877af39e7bc
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-01 13:05:05 +0300

    PKC added

--- kcore/ballotcompact-final/inc/common.h
+++ kcore/ballotcompact-final/inc/common.h
@@ -14,7 +14,7 @@
 #define UINT unsigned int
 #define DS_LOC string("../data_set/data/")
 #define OUTPUT_LOC string("../output/")
-#define REP 5
+#define REP 10
 
 #include <iostream>
 #include <vector>
