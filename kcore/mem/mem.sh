a25353b785fe38031128f8d945363569a3d41392
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-01 23:40:42 +0300

    PKC added

--- kcore/mem/mem.sh
+++ kcore/mem/mem.sh
@@ -9,28 +9,28 @@ do
 done
 
 
-fn="memlog"
-rm $fn
-while true;
-do
-    nvidia-smi | grep 'python3' | awk '{print $8}' | grep -o -E '[0-9]+' >> $fn
-    sleep $1
-done
+# fn="memlog"
+# rm $fn
+# while true;
+# do
+#     nvidia-smi | grep 'python3' | awk '{print $8}' | grep -o -E '[0-9]+' >> $fn
+#     sleep $1
+# done
 
 
-cd $(dirname $0) # Jalal: the folder containing the script will be the root folder. https://askubuntu.com/a/368100
-total_max=0
-f="memlog"
+# cd $(dirname $0) # Jalal: the folder containing the script will be the root folder. https://askubuntu.com/a/368100
+# total_max=0
+# f="memlog"
 
-max=`awk 'BEGIN{a=0}{if ($1>0+a) a=$1} END{print a}' $f`;
-min=0;  #`awk 'BEGIN{a=9999999}{if ($1<0+a) a=$1} END{print a}' $f`;
+# max=`awk 'BEGIN{a=0}{if ($1>0+a) a=$1} END{print a}' $f`;
+# min=0;  #`awk 'BEGIN{a=9999999}{if ($1<0+a) a=$1} END{print a}' $f`;
 
-dif="$(($max-$min))";
-if [ "$dif" -gt "$total_max" ]; then
-    total_max=$dif
-fi
-    #echo $dif;
+# dif="$(($max-$min))";
+# if [ "$dif" -gt "$total_max" ]; then
+#     total_max=$dif
+# fi
+#     #echo $dif;
 
-    # -f: force, "delete the file if exist, no error if doesn't"
-rm -f $f;
-echo "mem(MB): $total_max"
+#     # -f: force, "delete the file if exist, no error if doesn't"
+# rm -f $f;
+# echo "mem(MB): $total_max"
