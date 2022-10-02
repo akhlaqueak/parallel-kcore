533486b789bc6d4e7bacc4f0bcea6b061f14dcc5
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-03 00:27:33 +0300

    PKC added

--- kcore/vetga/mem.sh
+++ kcore/vetga/mem.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# while true;
+# do
+#         nvidia-smi --query-gpu=memory.used --format=csv,nounits,noheader
+#         sleep 0.01; 
+# done
+
+fn=$1
+rm $fn
 while true;
 do
-        nvidia-smi --query-gpu=memory.used --format=csv,nounits,noheader
-        sleep 0.01; 
+    nvidia-smi | grep 'python3' | awk '{print $8}' | grep -o -E '[0-9]+' >> $fn
+    sleep 0.001
 done
