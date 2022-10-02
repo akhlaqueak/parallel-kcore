1debbe09221d945e9a6beb4d7ea05502c486b757
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-02 12:20:00 +0300

    PKC added

--- kcore/PKC/runSimulationRepeated.py
+++ kcore/PKC/runSimulationRepeated.py
@@ -15,134 +15,37 @@ import statistics as stat
 # datasets = ['amazon0601.txt',   'cit-Patents.txt',     'in-2004.txt',         'patentcite.txt',        'uk-2002.txt',  'web-BerkStan.txt',  'wiki-Talk.txt', 'arabic-2005.txt',  'dblp-author.txt',     'indochina-2004.txt',  'uk-2005.txt',  'web-Google.txt', 'as-skitter.txt',   'hollywood-2009.txt', 'it-2004.txt',         'soc-LiveJournal1.txt',  'wb-edu.txt',   'webbase-2001.txt']
 datasets = [
 'amazon0601.txt', \
-'arabic-2005.txt', \
-'as-skitter.txt', \
-'dblp-author.txt', \
-'hollywood-2009.txt', \
-'in-2004.txt', \
-'indochina-2004.txt', \
-'it-2004.txt', \
-'patentcite.txt', \
-# 'sk-2005.txt', \
-'soc-LiveJournal1.txt', \
-'trackers.txt', \
-'uk-2002.txt', \
-'uk-2005.txt', \
-'wb-edu.txt', \
-'webbase-2001.txt', \
-'web-BerkStan.txt', \
-'web-Google.txt', \
-'wikipedia-link-de.txt', \
 'wiki-Talk.txt', \
+'web-Google.txt', \
+'web-BerkStan.txt', \
+'as-skitter.txt', \
+'patentcite.txt', \
+'in-2004.txt', \
+'dblp-author.txt', \
+'wb-edu.txt', \
+'soc-LiveJournal1.txt', \
+'wikipedia-link-de.txt', \
+'hollywood-2009.txt', \
+'com-orkut.txt', \
+'trackers.txt', \
+'indochina-2004.txt', \
+'uk-2002.txt', \
+'arabic-2005.txt', \
+'uk-2005.txt', \
+'webbase-2001.txt', \
+'it-2004.txt', \
 ]
 OUTPUT = "../output/"
 DATASET = "../data_set/data/"
 VERIFY = False
 VERBOSE = True
 NITERATIONS = 1
-
-def parseResult(output):
-    # One of the line in output has this format
-    # Elapsed Time: 69.8055
-    lines = [x for x in output.split("\n") if "time" in x]
-    res  = {}
-    # BZ time:     0.083 sec
-    for line in lines:
-        type, time = line.split(":")
-        time = float(time.split()[0])
-        res[type] = time
-    print (res)
-    return res
-
-def parseDataSet(args):
-        #  "twitter.g", "livejournal-groupmemberships.g", "yahoo-song.g", "bag-pubmed.g", \
-     	# "dbpedia-link.g", "wikipedia_link_ms.g", "dimacs10-uk-2002.g")
-    ds = []
-    arg = "" if len(args)<2 else args[1]
-
-    if arg == "":
-        ds.append("Enron.g")
-    elif arg.isnumeric():
-        ind = int(arg)
-        if(ind<len(datasets)):
-            ds.append(datasets[int(arg)])
-        else:
-            print("Please provide valid dataset: ", [(x, y) for x, y in enumerate(datasets)])
-    elif arg == "all":
-        ds = datasets
-    else:
-        ds.append(arg) 
-    return ds
-
-def runSimulation(datasets):
-
-    results = {}
-    for ds in datasets:
-        print(ds, ": Started... ", end=" ", flush=True)
-        # OMP_NUM_THREADS=32 ./pkc.exe ../data_set/data/in-2004.txt 
-        os.environ['OMP_NUM_THREADS'] = "32"
-        output = sp.run([ "./pkc.exe", "../data_set/data/" + ds], stdout=PIPE, stderr=PIPE)
-        text = output.stderr.decode()
-
-        if(VERBOSE): 
-            print("output: ", text, output.stdout.decode())
-        print ("======================================================")
-        time = parseResult(text) # decode is converting byte string to regular
-        results[ds] = time
-        print("Completed")
-    return results
+for ds in datasets:
+    print(ds, ": Started... ", end=" ", flush=True)
+    # OMP_NUM_THREADS=32 ./pkc.exe ../data_set/data/in-2004.txt 
+    os.environ['OMP_NUM_THREADS'] = "32"
+    sp.run([ "./pkc.exe", "../data_set/data/" + ds])
 
 
 
-
-
-def repeatedSimulations(datasets):
-    execTime = {}
-    verResult = {}
-    for ds in datasets:
-        execTime[ds] = []
-        verResult[ds] = []
-    
-    for i in range(NITERATIONS):
-        print("Running Simulation No. ", i+1)
-        exec = runSimulation(datasets)
-        for ds in datasets:
-            execTime[ds].append(exec[ds])
-    print("###---------------###")
-    print("###- Execution Time -###")
-    types = execTime[datasets[0]][0].keys()
-    print("datasets ", end=" ")
-    for type in types:
-        tp = type[:-5].replace(" ", "")
-        for i in range(NITERATIONS):
-            print(tp + str(i), end=" ")
-        print(tp+"avg", end=" ")
-
-    print()
-    for ds in datasets:
-        print(ds, end=" ")
-        for t in types:
-            sum = 0
-            for i in range(NITERATIONS):
-                time = execTime[ds][i][t]
-                print(time, end=" ")
-                sum += time
-            # print(sum/NITERATIONS, end=" ")
-        # print(stat.mean(execTime[ds]))
-        print()
-    print("###---------------###")
-
-        
-
-if __name__ == "__main__":
-    sp.run(["git", "pull"])
-    if len(sys.argv) == 1:
-        print("usage: ")
-        print("python runSimulation.py datasetIndex|all folderIndex \n")
-        print("datasetIndex: ", [str(x)+ ": " + y for x, y in enumerate(datasets)])
-        print("folderIndex: ", [str(x)+ ": " + y  for x, y in enumerate(folders)])
-        exit(0)
-    datasets = parseDataSet(sys.argv)
-
-    repeatedSimulations(datasets)
 
