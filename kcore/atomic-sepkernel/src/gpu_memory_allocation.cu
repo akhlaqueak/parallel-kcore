88900e06a2157e7d2231771c3daacc023cc5d26d
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-14 21:37:08 +0300

    degorder

--- kcore/atomic-sepkernel/src/gpu_memory_allocation.cu
+++ kcore/atomic-sepkernel/src/gpu_memory_allocation.cu
@@ -20,7 +20,7 @@ void malloc_graph_gpu_memory(Graph &g,G_pointers &p){
 }
 
 void get_results_from_gpu(Graph &g,G_pointers &p){
-    chkerr(cudaMemcpy(g.degrees,p.degrees,(g.V)*sizeof(unsigned int),cudaMemcpyDeviceToHost));    
+    chkerr(cudaMemcpy(g.degrees,p.degOrder,(g.V)*sizeof(unsigned int),cudaMemcpyDeviceToHost));    
 }
 
 void free_graph_gpu_memory(G_pointers &p){
