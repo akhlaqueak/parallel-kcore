ba6030a449177e86146108ea8d7de6775a8fe110
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-16 00:18:46 +0300

    degorder

--- kcore/BK/main.cu
+++ kcore/BK/main.cu
@@ -80,6 +80,7 @@ int find_kcore(Graph &g,bool write_to_disk){
 
     for (int i=0; i<V; i+=BLK_NUMS*SUBG){
         BK<<<BLK_NUMS, BLK_DIM>>>(dp, sg, i);
+        cout<<"HI"<<endl;
     }
 
     return chrono::duration_cast<chrono::milliseconds>(chrono::steady_clock::now() - tick).count();
