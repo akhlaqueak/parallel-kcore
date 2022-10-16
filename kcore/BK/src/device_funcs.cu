96ebb13a97cc5e22fb8e78d5ea68f99b11a20ba1
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-16 13:55:48 +0300

    degorder

--- kcore/BK/src/device_funcs.cu
+++ kcore/BK/src/device_funcs.cu
@@ -49,7 +49,7 @@ __global__ void BK(G_pointers dp, Subgraphs* subgs, unsigned int base){
     for(int i=0;i<otail;i+=2){
         unsigned int st = sg.offsets[i];
         unsigned int en = sg.offsets[i+1];
-        printf("%d-$d:", st, edn);
+        printf("%d-$d:", st, en);
         for(;st<en;st++){
             printf("%d%c ", sg.vertices[st], sg.labels[st]);
         }
