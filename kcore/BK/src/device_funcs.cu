b98d1f9a1a06bf653b37600ffe20296b8eff589c
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-17 23:52:17 +0300

    repo

--- kcore/BK/src/device_funcs.cu
+++ kcore/BK/src/device_funcs.cu
@@ -3,17 +3,16 @@
 #include "stdio.h"
 #include "buffer.cc"
 #include "kcore.cc"
-
+#include "util.cc"
 __device__ int generateSubGraphs(G_pointers dp, Subgraphs sg, 
         unsigned int v){
     unsigned int laneid = LANEID;
-    unsigned int warpid = WARPID;
     unsigned int* otail = sg.otail;
     unsigned int* vtail = sg.vtail;
     unsigned int start = dp.neighbors_offset[v];
     unsigned int end = dp.neighbors_offset[v+1];
     unsigned int len = end-start+1; // number of neighbors + v itself
-    if(len==1) return; // there was no neighbor for this vertex... 
+    if(len==1) return 0; // there was no neighbor for this vertex... 
     unsigned int loc, u, ot;
     if(laneid == 0){
         loc = atomicAdd(vtail, len);
@@ -36,10 +35,9 @@ __device__ int generateSubGraphs(G_pointers dp, Subgraphs sg,
 }
 
 __device__ void fixR(Subgraphs sg, unsigned int ot, unsigned int st, unsigned int en){
-    unsigned int sgst = sg.offset[ot];
-    unsigned int sgen = sg.offset[ot+1];
+    unsigned int sgst = sg.offsets[ot];
+    unsigned int sgen = sg.offsets[ot+1];
     unsigned int laneid=LANEID;
-    unsigned int k;
     for(unsigned int i=sgst; i<sgen; i++){
         if(sg.labels[i]==R) continue; // this node already have status R
         unsigned int v = sg.vertices[i];
@@ -58,21 +56,20 @@ __device__ void expandClique(G_pointers dp, Subgraphs sg, unsigned int i,  unsig
     unsigned int st = sg.otail[i];
     unsigned int en = sg.otail[i+1];
 
-    unsigned int warpid=WARPID;
-    unsigned int laneid=LANEID;
-    unsigned int v, pst, pen, k, u, pred;
+    // unsigned int warpid=WARPID;
+    // unsigned int laneid=LANEID;
+    unsigned int v, pst, pen;
     pst = dp.neighbors_offset[pivot];
     pen = dp.neighbors_offset[pivot+1];
-    unsigned int sum;
     // subgraph stored in (st, en)
     // N(pivot) are in (pst, pen)
     for(unsigned int i = st; i<en; i++){
         // entire warp is reading one vertex from subgraph, hence not adding laneid in i
         if(sg.labels[i]!=P) continue;
         v = sg.vertices[i]; // v in P
-        sum = countAtWarp(dp.neighbors, pst, pen, v);
+        
 
-        if(sum==0){ //this v is in P-N(pivot)
+        if(!searchAtWarp(dp.neighbors, pst, pen, v, ANY)){ //this v is in P-N(pivot)
             // generate a subgraph for this sg.vertices[i]: done
             unsigned int ot = generateSubGraphs(dp, sg, v);
             // fux R in newly spawned graph, all such vertices which are R in old graph are R in spawned graph
@@ -82,24 +79,24 @@ __device__ void expandClique(G_pointers dp, Subgraphs sg, unsigned int i,  unsig
 }
 
 __device__ bool examineClique(Subgraphs sg, unsigned int i){
-    unsigned int warpid=WARPID;
     unsigned int laneid=LANEID;
     unsigned int st, en, j, res;
     bool pred, report;
     st = sg.offsets[i];
     en = sg.offsets[i+1];
-    report = true;
-    for(; st<en; st+=32){
-        j = st+laneid;
-        if(j>=en) pred = true; // these are non-participating threads, hence getting value true
-        else pred = sg.labels[j]==R;
-        res = __ballot_sync(FULL, pred);
-        if(res!=FULL){
-            report = false;
-            break;
-        }
-    }
-    return report;
+    return searchAtWarpAll(sg.labels, st, en, R);
+    // report = true;
+    // for(; st<en; st+=32){
+    //     j = st+laneid;
+    //     if(j>=en) pred = true; // these are non-participating threads, hence getting value true
+    //     else pred = sg.labels[j]==R;
+    //     res = __ballot_sync(FULL, pred);
+    //     if(res!=FULL){
+    //         report = false;
+    //         break;
+    //     }
+    // }
+    // return report;
 }
 
 
@@ -122,11 +119,11 @@ __device__ unsigned int selectPivot(G_pointers dp, Subgraphs sg, unsigned int i)
         st1 = dp.neighbors_offset[v];
         en1 = dp.neighbors_offset[v+1];
         nmatched = 0, max = 0, pivot = v;
-        for(unsigned int k=st; k<end; k+=32){
+        for(unsigned int k=st; k<en; k+=32){
             unsigned int kl = k+laneid; // need to run all lanes, so that ballot function works well
             pred = false;
             // some of the lanes will diverge from this point, it may be improved in future.
-            if(kl<end && sg.labels[kl]==P)  // only P nodes will be searched.
+            if(kl<en && sg.labels[kl]==P)  // only P nodes will be searched.
                 pred = binarySearch(dp.neighbors+st1, en1-st1, sg.vertices[kl]); // P intersect N(v)
                 // binary search can introduce divergence, we can also try with warp level linear search in future
             nmatched+=__popc(__ballot_sync(FULL, pred));
@@ -152,6 +149,7 @@ __global__ void BK(G_pointers dp, Subgraphs* subgs, unsigned int base){
 
     unsigned int warpid = WARPID;
     unsigned int laneid = LANEID;
+    unsigned int s;
     if(THID==0){
         sg = subgs[BLKID];
         sg.otail = &otail;
@@ -176,7 +174,7 @@ __global__ void BK(G_pointers dp, Subgraphs* subgs, unsigned int base){
         ohead = min(otail, ohead+WARPS_EACH_BLK);
         __syncthreads();
 
-        if(examineClique(sg, i)){
+        if(examineClique(sg, s)){
             // todo report clique
             // ?? do we need to store R, or just increment a count
             // seemingly GPU-BK(TPDS) is only calculating number of cliques
--- kcore/BK/src/util.cc
+++ kcore/BK/src/util.cc
@@ -9,17 +9,48 @@ __device__ bool binarySearch(unsigned int* arr, unsigned int end, unsigned int v
     return false;
 }
 // counts occurance of v in data[st:en]
-__device__ int countAtWarp(unsigned int* data, unsigned int st, unsigned int en, unsigned int v){
+// __device__ int countAtWarp(unsigned int* data, unsigned int st, unsigned int en, unsigned int v){
+//     bool pred;
+//     unsigned int laneid = LANEID;
+//     unsigned int sum = 0;
+//     for(; st<en; st+=32){
+//         k = st+laneid;
+//         pred = false;
+//         // not applying binary search here, as it may diverge the warp
+//         if(k < en)
+//             pred = (v == data[k]);
+//         sum += __popc(__ballot_sync(FULLl, pred));
+//     }
+//     return sum;
+// }
+__device__ int searchAtWarpAny(auto* data, unsigned int st, unsigned int en, auto v){
     bool pred;
     unsigned int laneid = LANEID;
-    unsigned int sum = 0;
-    for(; st<en; st+=32){
+    unsigned int res;
+    for(unsigned int k; st<en; st+=32){
         k = st+laneid;
         pred = false;
         // not applying binary search here, as it may diverge the warp
         if(k < en)
             pred = (v == data[k]);
-        sum += __popc(__ballot_sync(FULLl, pred));
+        res = __ballot_sync(FULL, pred);
+        if(res!=0) return true;
     }
-    return sum;
+    return false;
+}
+
+__device__ int searchAtWarpAll(auto* data, unsigned int st, unsigned int en, auto v){
+    bool pred;
+    unsigned int laneid = LANEID;
+    unsigned int res;
+    for(unsigned int k; st<en; st+=32){
+        k = st+laneid;
+        pred = true;
+        // not applying binary search here, as it may diverge the warp
+        if(k < en)
+            pred = (v == data[k]);
+        res = __ballot_sync(FULL, pred);
+        if(res!=FULL) return false;
+    }
+    return true;
 }
