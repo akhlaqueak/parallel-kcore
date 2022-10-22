77734a60cbbd8283544b0144563e9e70381d70b7
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-22 16:09:05 +0300

    temp buffer added

--- kcore/BK/src/bk.cu
+++ kcore/BK/src/bk.cu
@@ -222,7 +222,7 @@ __global__ void BK(G_pointers dp, Subgraphs* subgs, unsigned int base){
         ohead = min(otail, ohead+WARPS_EACH_BLK*2);
         __syncthreads();
         if(s>=otail) continue;
-        // if(THID==0)printf("##");
+        if(THID==0)printf("##");
         if(examineClique(sg, s)){
             // todo report clique
             // ?? do we need to store R, or just increment a count
