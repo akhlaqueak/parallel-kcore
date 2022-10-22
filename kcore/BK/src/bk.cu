f515a72068f4ce5820d9c9e6b944db475d1f842c
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-22 14:44:11 +0300

    temp buffer added

--- kcore/BK/src/bk.cu
+++ kcore/BK/src/bk.cu
@@ -161,10 +161,10 @@ __device__ unsigned int selectPivot(G_pointers dp, Subgraphs sg, unsigned int i)
             pred = false;
             // some of the lanes will diverge from this point, it may be improved in future.
             if(kl<en && sg.labels[kl]==P)  // only P nodes will be searched.
-            pred = binarySearch(dp.neighbors+st1, en1-st1, sg.vertices[kl]); // P intersect N(v)
+                pred = binarySearch(dp.neighbors+st1, en1-st1, sg.vertices[kl]); // P intersect N(v)
             // binary search can introduce divergence, we can also try with warp level linear search in future
-            nmatched+=__popc(__ballot_sync(FULL, pred));
             printf("%d:%d-", st, en);
+            nmatched+=__popc(__ballot_sync(FULL, pred));
         }
         if(nmatched > max){
             max = nmatched;
