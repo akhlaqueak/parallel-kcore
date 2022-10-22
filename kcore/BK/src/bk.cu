1b6de1431a79bddc01777ddba9f319875e138b05
akhlaqueak <akhlaque.ak@gmail.com>
2022-10-22 14:18:31 +0300

    temp buffer added

--- kcore/BK/src/bk.cu
+++ kcore/BK/src/bk.cu
@@ -18,7 +18,7 @@ __device__ int initializeSubgraph(Subgraphs sg, unsigned int len, unsigned int v
     unsigned int* vtail = sg.vtail;
     unsigned int* otail = sg.otail;
     unsigned int ot, vt=0;
-    if(LANEID == 0){
+    if(threadIdx.x&31 == 0){
         printf("%d@", vt); 
         vt = atomicAdd(vtail, len);
         ot = atomicAdd(otail, 2);
