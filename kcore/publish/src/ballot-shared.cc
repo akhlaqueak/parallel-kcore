45025f86db0c6b002a007a2cd7207f85de702b3b
akhlaqueak <akhlaque.ak@gmail.com>
2022-12-28 18:34:47 +0300

    shared memory added to ballot and efficient scans

--- kcore/publish/src/ballot-shared.cc
+++ kcore/publish/src/ballot-shared.cc
@@ -45,7 +45,8 @@ __global__ void processNodes8(G_pointers d_p, int level, int V,
     unsigned int regTail;
     unsigned int i;
     if(THID==0){
-        initTail = bufTail = bufTails[blockIdx.x];
+        bufTail = bufTails[blockIdx.x];
+        initTail = bufTail;
         base = 0;
         glBuffer = glBuffers + blockIdx.x*GLBUFFER_SIZE; 
         assert(glBuffer!=NULL);
