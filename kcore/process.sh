1d711b7795928f77dc945607d8b0cc385c2dfa92
akhlaqueak <akhlaque.ak@gmail.com>
2022-12-29 00:40:53 +0300

    parsing results

--- kcore/process.sh
+++ kcore/process.sh
@@ -1,16 +1,9 @@
-res=["Dataset"
-"Ours"
-"Ours + shared Mem"
-"Ours + Vertex Prefetching:"
-"Efficient scan:"
-"Ballot scan:"
-"Efficient scan + Shared Memory + :"
-"Ballot scan + Shared Memory:"
-"Efficient Scan + Vertex Prefetching:"
-"Ballot scan + Vertex Prefetching:"
-]
+res=(
+Ours	"Ours +  SM"	"Ours + VP"	"Ballot scan"	"Ballot scan + SM"	"Ballot scan + 
+VP"	"Efficient scan"	"Efficient scan + SM"	"Efficient Scan + VP"
+)
 
-for ds in $res; do
-    echo "$ds\n\n\n"
+for ds in "${res[@]}"; do
+    echo "$ds"
     grep "$ds" log/publish.out | awk '{print $NF}'
 done
