407b496dd0d70615a72500e734f26141787517ca
akhlaqueak <akhlaque.ak@gmail.com>
2023-01-02 02:22:28 +0300

    adding prefetch2 version

--- kcore/publish/main.cu
+++ kcore/publish/main.cu
@@ -9,6 +9,7 @@
 #include "./src/ours.cc"
 #include "./src/ours-shared.cc"
 #include "./src/ours-prefetch.cc"
+#include "./src/ours-prefetch2.cc"
 #include "./src/efficient.cc"
 #include "./src/ballot-prefetch.cc"
 #include "./src/ballot.cc"
