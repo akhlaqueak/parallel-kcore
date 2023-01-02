3530d0d358295e86c9684c3818591c05594bbe05
akhlaqueak <akhlaque.ak@gmail.com>
2023-01-02 13:17:32 +0300

    adding prefetch2 version

--- kcore/publish/src/ours-prefetch2.cc
+++ kcore/publish/src/ours-prefetch2.cc
@@ -234,7 +234,7 @@ int kcorePrefetch2(Graph &data_graph)
     // cout<<"K-core Computation Started";
 
     auto start = chrono::steady_clock::now();
-    while (count < data_graph.V && level < 20)
+    while (count < data_graph.V)
     {
         cudaMemset(bufTails, 0, sizeof(unsigned int) * BLK_NUMS);
 
